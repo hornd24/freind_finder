13918e6e034e2953728426c9662ec1c39eb0c0c4
hornd24 <david.horn689@gmail.com>
2017-12-05 10:20:19 -0800

    lol

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ routs(app);
 app.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname, "public/survey.html"));
 });
+var compareNum=[];
 var compare=[];
 var newfreind = [];
 var friendsList = [{
@@ -27,72 +28,96 @@ var friendsList = [{
         photo: 'https://vignette.wikia.nocookie.net/southpark/images/5/58/Cartman-no-hat-0.png/revision/latest/scale-to-width-down/310?cb=20170725145528',
         questions: [2, 5, 4, 3, 1, 3, 5, 4, 5, 1]
     },
-    {
-        name: 'Butters Stotch',
-        photo: 'https://vignette.wikia.nocookie.net/southpark/images/0/06/ButtersStotch.png/revision/latest/scale-to-width-down/310?cb=20170322004353',
-        questions: [5, 5, 5, 5, 3, 2, 4, 5, 3, 2, 5]
+    {name: 'Stan Marsh',
+    photo: 'https://vignette4.wikia.nocookie.net/southpark/images/2/2a/1105_chasedbyrabbits.JPG/revision/latest?cb=20070901123200',
+    questions: [5, 3, 4, 3, 4, 3, 5, 3, 5, 4]
+       
     }, {
         name: 'Randy Marsh',
         photo: 'https://static.comicvine.com/uploads/original/11125/111254686/5113188-0906127496-CHKON.jpg',
         questions: [5, 5, 5, 3, 2, 4, 5, 3, 4, 5]
 
     },
-    {
-        name: 'Stan Marsh',
-        photo: 'https://vignette4.wikia.nocookie.net/southpark/images/2/2a/1105_chasedbyrabbits.JPG/revision/latest?cb=20070901123200',
-        questions: [5, 3, 4, 3, 4, 3, 5, 3, 5, 4]
+    {name: 'Butters Stotch',
+    photo: 'https://vignette.wikia.nocookie.net/southpark/images/0/06/ButtersStotch.png/revision/latest/scale-to-width-down/310?cb=20170322004353',
+    questions: [5, 5, 5, 3, 2, 4, 5, 3, 4, 5]
+        
     },
 ]
-
+// [5, 5, 5, 5, 3, 2, 4, 5, 3, 2, 5]
 app.post("/api/add", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body-parser middleware
     var answers = req.body;
     // console.log(answers)
 
-// function special(){
-//     this.
-// }
 
+console.log(friendsList.length)
+function Comparing(name,link,sum){
+    this.freindNam=name;
+    this.urlLink=link;
+    this.math=sum;
+}
 
     newfreind.push(answers);
+// console.log(friendsList[1]);
+
+var perfectName = '';
+var perfectPhoto = '';
 
 
     for (i = 0; i < newfreind.length; i++) {
         // console.log(newfreind[i].questions[i])
         if (answers.name == newfreind[i].name) {
-            var test = newfreind[i].questions.map(Number);
+            var avdata = newfreind[i].questions.map(Number);
+   
             // console.log(test)
-            //   console.log(friendsList[0].questions)
+                 }         //   console.log(friendsList[0].questions)
             for (b = 0; b < friendsList.length; b++) {
                 // console.log(b)
-                friendsList[b]
-                for (j = 0; j < friendsList[i].questions.length; j++) {
+            //   console.log(  friendsList[b])
+                for (j = 0; j < friendsList[b].questions.length; j++) {
                     sum = 0;
-                    
+                
                     //    console.log(friendsList[j].questions)
-                    Math.abs(sum += test[j] -friendsList[b].questions[j]);
+                    sum +=Math.abs(avdata[j] -friendsList[b].questions[j]);
+                   
                     // console.log(typeof sum)
                     // console.log(typeof friendsList[i].questions);
                     // console.log(typeof test[0])
-                    var sumabs = Math.abs(sum);
-                  
                    
                 }
+                // console.log(friendsList)
+              
                 
-                compare.push(sumabs);
+          var  comparing= new Comparing(friendsList[b].name,friendsList[b].photo,sum)
+
+          compare.push(comparing);    
+             
+         
+
+                
             }
+            
         }
-    }
-for(var i=0;i<compare.length;i++){
- var testing= Math.min(compare[i]);
+    
+    console.log(compare)
+   
+// for(var i=0;i<compare.length;i++){
+//    compareNum.push(compare[i].math);
 
-}
-compare.splice('');
-console.log(compare);
-console.log(testing);
+// var test =Math.min(compareNum[i])
 
-    res.json(answers);
+// }
+// // var testing= Math.min(compareNum);
+// console.log(compareNum)
+// console.log(test)
+
+// compare.splice('');
+
+// console.log(testing);
+
+    res.json({perfectName,perfectPhoto});
 });
 
 app.listen(PORT, function () {
